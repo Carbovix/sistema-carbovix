a36df529f77ce48496b43656b7d1b69911b69ecd
Seu Nome <seu@email.com>
2026-05-28 16:08:39 -0300

    feat: adiciona função de deletar dados

--- src/funcoes.py
+++ src/funcoes.py
@@ -1,4 +1,31 @@
 import src.menus as ui
+import csv
+import os
+
 
 def adiciiona_dado():
     return
+
+def deletar_dados():
+    numero_dado = input("Digite o registro do dado que deseja deletar: ")
+
+    arquivo_path = "data/dados.csv"
+    linhas_mantidas = []
+    encontrado = False
+    with open(arquivo_path, "r", newline="") as arquivo:
+        leitor_csv = csv.reader(arquivo)
+        for linha in leitor_csv:
+            if linha and linha[0] == numero_dado:
+                encontrado = True
+                continue
+            linhas_mantidas.append(linha)
+
+    if encontrado:
+        with open(arquivo_path, "w", newline="") as arquivo:
+            escritor_csv = csv.writer(arquivo)
+            escritor_csv.writerows(linhas_mantidas)
+            print(f"Dado com registro '{numero_dado}' foi deletado com sucesso.")
+    else:
+        print(f"O dado com registro '{numero_dado}' não foi encontrado.")
+        
+
