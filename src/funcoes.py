2396ee6cb59531431e5f8e762f4a7fb64dcafe13
João Souza <itsjvsouza@gmail.com>
2026-05-29 21:32:55 -0300

    feat: adiciona função de adicionar dados

--- src/funcoes.py
+++ src/funcoes.py
@@ -1,4 +1,40 @@
 import src.menus as ui
+import os
+import csv
 
-def adiciiona_dado():
-    return
+def adiciona_dado():
+    os.system("cls")
+
+    nome_maquina = input("Digite o nome da máquina: ")
+    qnt_emissao = input("Digite a quantidade emitida pela máquina: ")
+
+    linha = f"{nome_maquina}, {qnt_emissao}"
+
+    with open("data/dados.csv", "a", newline="") as arquivo:
+        arquivo.write(linha)
+
+    print("\nDado adionado com sucesso!")
+
+def deletar_dados():
+    numero_dado = input("Digite o registro do dado que deseja deletar: ")
+
+    arquivo_path = "data/dados.csv"
+    linhas_mantidas = []
+    encontrado = False
+    with open(arquivo_path, "r", newline="") as arquivo:
+        leitor_csv = csv.reader(arquivo)
+        for linha in leitor_csv:
+            if linha and linha[0] == numero_dado:
+                encontrado = True
+                continue
+            linhas_mantidas.append(linha)
+
+    if encontrado:
+        with open(arquivo_path, "w", newline="") as arquivo:
+            escritor_csv = csv.writer(arquivo)
+            escritor_csv.writerows(linhas_mantidas)
+            print(f"Dado com registro '{numero_dado}' foi deletado com sucesso.")
+    else:
+        print(f"O dado com registro '{numero_dado}' não foi encontrado.")
+        
+
